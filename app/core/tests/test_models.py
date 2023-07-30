01e733e66799408471f9ab01fad5799f7e149b36
namanarora3 <naman30103@gmail.com>
2023-07-31 05:28:53 +0530

    linting errors fixed

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -27,20 +27,20 @@ class ModelTests(TestCase):
         ''' test email is normalised'''
 
         sample_email = [
-            ['test1@EXAMPLE.com','test1@example.com'],
-            ['Test2@Example.com','Test2@example.com'],
-            ['TEST3@EXAMPLE.COM','TEST3@example.com'],
-            ['test4@example.COM','test4@example.com'],
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
         ]
 
         for email, expected in sample_email:
-            user = get_user_model().objects.create_user(email,'sample123')
+            user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
         ''' creating user without email address raises ValueError'''
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('','test@123')
+            get_user_model().objects.create_user('', 'test@123')
 
     def test_create_superuser(self):
         '''test creating a super user'''
