4c8bf01764d512c57c28f18f4fe0da9843bdfa3f
namanarora3 <naman30103@gmail.com>
2023-08-26 02:29:12 +0530

    Changed auth keyword to Bearer

--- app/app/settings.py
+++ app/app/settings.py
@@ -149,7 +149,7 @@ AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'app.authentication.BearerAuthentication',
         'rest_framework.authentication.SessionAuthentication'
     ),
 }
